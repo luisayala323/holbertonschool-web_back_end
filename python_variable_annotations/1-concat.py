e88c9efe9828fe241883ce8a91de9ca2cc5c9903
Luis <luisayala323@gmail.com>
2023-08-30 18:19:41 -0400

    concatenate 2 strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Concatenate 2 strings together.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates 2 input strings together."""
+    return str1 + str2
