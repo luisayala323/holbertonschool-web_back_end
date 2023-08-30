0b75944b78f47532788b9bf99621b30bf85a448b
Luis <luisayala323@gmail.com>
2023-08-30 18:17:17 -0400

    small fix end line space

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Function add that takes a float 'a' and a float 'b'
- as arguments and returns their sum as a float. """
- 
+as arguments and returns their sum as a float. """
+
+
 def add(a: float, b: float) -> float:
     """ The sum of the two input float numbers."""
     return a + b
