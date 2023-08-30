6f88794ac2a656c45e03cccf5316efc70590fe1d
Luis <luisayala323@gmail.com>
2023-08-30 18:34:26 -0400

    define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+The script has variables and functions
+ with  specified annotated values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
