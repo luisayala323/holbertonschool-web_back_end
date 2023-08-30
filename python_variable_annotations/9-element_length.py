16d7942e8102903084ebd5a8a9b6056e314239bd
Luis <luisayala323@gmail.com>
2023-08-30 18:28:23 -0400

    mistakes mistakes 9 to 3 !!

--- python_variable_annotations/9-element_length.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Function to_str that converts a float
- to its string representation"""
-
-
-def to_str(n: float) -> str:
-    """float convert to its string representation."""
-    z = str(n)
-    return z
