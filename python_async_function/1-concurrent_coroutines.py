40f35f320a1b59e09a03f741f88c07f99d8aa89e
Luis <luisayala323@gmail.com>
2023-08-31 12:33:14 -0400

    demonstrates asynchronous

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""Asynchronous Code Example"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Simulate asynchronous waiting for random durations.
+    """
+
+    delays = []
+    tasks = []
+
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+
+    return sorted(delays)
+
+if __name__ == "__main__":
+    import asyncio
+
+    print(asyncio.run(wait_n(5, 5)))
+    print(asyncio.run(wait_n(10, 7)))
+    print(asyncio.run(wait_n(10, 0)))
