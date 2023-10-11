4365350aade4d7135d8e222ae95341c738cd7a48
Luis <luisayala323@gmail.com>
2023-10-11 15:37:44 -0400

    .

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,13 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error()).finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => {
+      console.error('Got an error from the API');
+      return new Error('API request failed');
+    })
+ 
 }
