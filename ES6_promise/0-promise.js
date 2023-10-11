195a1e68b12bb9af9a3b18709f9a7ad21b9a922a
Luis <luisayala323@gmail.com>
2023-10-11 14:57:31 -0400

    0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
-/* promise has to have a function to work */
-
+/* promise has to have a function to work
+ eslint-disable */
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => [resolve, reject]);
+    return new Promise(() => {});
   }
-  
