97f3060b49efacf2b9dbf001425845149afe60df
Luis <luisayala323@gmail.com>
2023-10-13 22:42:38 -0400

    .

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,19 @@
 /* enlist-disable */
-function cleanSet(set) {
-  const cleanedSet = new Set();
-  set.forEach((item) => {
-    if (item.trim() !== '') {
-      cleanedSet.add(item.trim());
+function cleanSet(set, startString) {
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
+  const filteredValues = new Set();
+
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const restOfValue = value.slice(startString.length);
+      filteredValues.add(restOfValue);
     }
-  });
-  return cleanedSet;
+  }
+
+  return Array.from(filteredValues).join('-');
 }
 
 export default cleanSet;
